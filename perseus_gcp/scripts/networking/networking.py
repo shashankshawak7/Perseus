76fd067927cb1691b1a4087a5f71d355fc17529c
shashank shawak <shashankshawak7@gmail.com>
2021-05-08 01:44:37 +0530

    configuration for vpc-firewall-dns-policy

---
+++ perseus_gcp/scripts/networking/networking.py
@@ -0,0 +1,24 @@
+""" this code will perform all the networking needs without the fuss to learn them all from cloud"""
+import json
+import os
+
+class networkingGCP:
+
+
+    def __init__(self, config, version):
+        self.config = config
+        self.version = version
+        self.initurl = f'https://www.googleapis.com/compute/{version}/projects/'
+
+    def readconfig(self, what_to_create ):
+        self.configfilename = f'create{what_to_create}.cfg'
+        self.configlocation = os.path.join('./', 'configuration', self.configfilename)
+        with open(self.configlocation,'r') as handler:
+            data = handler.read()
+        data_read = json.loads(data)
+        return data_read
+
+    def __createvpc(self,):
+        pass
+            
+            
